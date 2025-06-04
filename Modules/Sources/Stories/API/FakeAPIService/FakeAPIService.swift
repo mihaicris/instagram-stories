b2b2f18503a31117aeffd32fdf46b53913c4f194
Mihai Cristescu <mihai.cristescu@gmail.com>
2025-06-04 22:18:26 +0300

    Updates

--- Modules/Sources/Stories/API/FakeAPIService/FakeAPIService.swift
+++ Modules/Sources/Stories/API/FakeAPIService/FakeAPIService.swift
@@ -9,8 +9,8 @@ public struct FakeAPIService: APIService {
     _ request: URLRequest,
     of _: T.Type,
     decoder _: JSONDecoder
-  ) throws -> T {
-    // try await Task.sleep(nanoseconds: 2_000_000_000) // Simulate network delay
+  ) async throws -> T {
+    try await Task.sleep(nanoseconds: 1_000_000_000) // Simulate network delay
 
     let result: T? =
       fakeGetUsers(request) ?? fakeGetStory(request)
