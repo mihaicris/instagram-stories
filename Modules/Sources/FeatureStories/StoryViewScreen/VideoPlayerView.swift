2a3b5480731ae4240dff6dff3ba84285f521d042
Mihai Cristescu <mihai.cristescu@gmail.com>
2025-06-13 14:00:32 +0300

    Updates

--- Modules/Sources/FeatureStories/StoryViewScreen/VideoPlayerView.swift
+++ Modules/Sources/FeatureStories/StoryViewScreen/VideoPlayerView.swift
@@ -70,4 +70,35 @@ struct VideoPlayerView: View {
     private func removeEndObserver() {
         NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
     }
+
+    private struct CustomVideoPlayerView: UIViewRepresentable {
+        let player: AVPlayer
+
+        func makeUIView(context _: Context) -> PlayerView {
+            let view = PlayerView()
+            view.player = player
+            return view
+        }
+
+        func updateUIView(_: PlayerView, context _: Context) {}
+
+        class PlayerView: UIView {
+            override class var layerClass: AnyClass {
+                AVPlayerLayer.self
+            }
+
+            var playerLayer: AVPlayerLayer {
+                // swiftlint:disable:next force_cast
+                layer as! AVPlayerLayer
+            }
+
+            var player: AVPlayer? {
+                get { playerLayer.player }
+                set {
+                    playerLayer.player = newValue
+                    playerLayer.videoGravity = .resizeAspectFill
+                }
+            }
+        }
+    }
 }
