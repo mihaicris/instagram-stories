ce774dbf2f6cf440ba8acb9adecaf77527521807
Mihai Cristescu <mihai.cristescu@gmail.com>
2025-06-15 13:41:01 +0300

    Updates

--- Main/InstagramApp.swift
+++ Main/InstagramApp.swift
@@ -16,8 +16,8 @@ struct InstagramApp: App {
 
     var body: some Scene {
         WindowGroup {
-            // StoryListScreen(model: .init())
-            TestView(model: .init())
+            StoryListScreen(model: .init())
+            // TestView(model: .init())
         }
     }
 
